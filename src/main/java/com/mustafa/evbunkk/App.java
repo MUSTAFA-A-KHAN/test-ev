3ef80450f682b9508e7ca58c21e060cf7e149248
sabrina <khansabrina1298@gmail.com>
2022-09-04 15:23:02 +0530

    initial commit

---
+++ src/main/java/com/mustafa/evbunkk/App.java
@@ -0,0 +1,67 @@
+package com.mustafa.evbunkk;
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.http.HttpResponse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginservlet
+ */
+@WebServlet("/loginservlet")
+public class App extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public App() { 
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+    	System.out.println("do Post of login");
+
+		RequestDispatcher rd=request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);	// TODO Auto-generated method stub
+
+		}
+		@Override
+		public void destroy() {
+			System.out.println("destroy called");
+		}
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			super.doGet(request, response);
+			response.setContentType("text/html");
+			PrintWriter out=response.getWriter();
+			out.println("<form name=\"newloginform\" method=\"post\" action=\"loginservlet\">\r\n"
+					+ "\r\n"
+					+ "	 Usernameinlogin:<input type=\"text\" name=\"username\"/><br>\r\n"
+					+ "	 Passwordin login:<input type=\"password\" name=\"password\"/> <br>\r\n"
+					+ "	<input type=\"submit\" value=\"sessionout\"/>\r\n"
+					+ "</form>");
+			System.out.println("dogetcalled");
+			
+			RequestDispatcher rd=request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);
+		}
+		
+		
+		
+	}
+
+
